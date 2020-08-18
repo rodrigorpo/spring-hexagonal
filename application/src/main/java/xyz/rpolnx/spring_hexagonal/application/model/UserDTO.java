e9190206c579d23309d3c1de66384077441c3aff
Rodrigo Pereira <rodrigorpogo@gmail.com>
2020-08-18 15:26:36 -0300

    UserDTO

---
+++ application/src/main/java/xyz/rpolnx/spring_hexagonal/application/model/UserDTO.java
@@ -0,0 +1,30 @@
+package xyz.rpolnx.spring_hexagonal.application.model;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+import static lombok.AccessLevel.PRIVATE;
+
+import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import xyz.rpolnx.spring_hexagonal.domain.model.User;
+
+@AllArgsConstructor(access = PRIVATE)
+@NoArgsConstructor
+@Data
+@JsonInclude(NON_NULL)
+public class UserDTO {
+    public UUID id;
+    private String name;
+
+    public User toUser() {
+        return new User(id, name);
+    }
+
+    public static UserDTO fromUser(User user) {
+        return new UserDTO(user.getId(), user.getName());
+    }
+}
