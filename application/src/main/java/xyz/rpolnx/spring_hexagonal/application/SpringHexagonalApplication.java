b81c11cd80f81873a48750c439685472c3b11305
Rodrigo Pereira <rodrigorpogo@gmail.com>
2020-08-18 14:14:13 -0300

    Module structure

--- application/src/main/java/xyz/rpolnx/spring_hexagonal/application/SpringHexagonalApplication.java
+++ application/src/main/java/xyz/rpolnx/spring_hexagonal/application/SpringHexagonalApplication.java
@@ -1,4 +1,4 @@
-package xyz.rpolnx.spring_hexagonal;
+package xyz.rpolnx.spring_hexagonal.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringHexagonalApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringHexagonalApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHexagonalApplication.class, args);
+    }
 
 }
