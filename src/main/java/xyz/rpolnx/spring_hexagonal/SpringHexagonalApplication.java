6d49737db7439455d702d54505a22c5baed5f754
Rodrigo Pereira <rodrigorpogo@gmail.com>
2020-08-18 13:38:35 -0300

    Creating structure

---
+++ src/main/java/xyz/rpolnx/spring_hexagonal/SpringHexagonalApplication.java
@@ -0,0 +1,13 @@
+package xyz.rpolnx.spring_hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHexagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHexagonalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/rpolnx/spring_hexagonal/SpringHexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.rpolnx.spring_hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
